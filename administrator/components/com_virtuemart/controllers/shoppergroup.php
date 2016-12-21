001068637bc479a284971901d0fe6499409e768f
B-Seite <boris@b-seite.de>
2016-12-21 12:08:18 +0100

    Shoppergroup ‚additional Shopper‘ Toggle Functionality added

--- administrator/components/com_virtuemart/controllers/shoppergroup.php
+++ administrator/components/com_virtuemart/controllers/shoppergroup.php
@@ -19,8 +19,7 @@
 // Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die('Restricted access');
 
-if(!class_exists('VmController'))require(VMPATH_ADMIN.DS.'helpers'.DS.'vmcontroller.php');
-
+JLoader::register('VmController', VMPATH_ADMIN .'/helpers/vmcontroller.php');
 
 /**
  * Shopper Group Controller
